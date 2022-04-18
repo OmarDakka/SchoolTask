68da18b60e1d628a6e8624f0dddfc8d6554c17f8
OmarDakka <omardakka42@gmail.com>
2022-04-18 16:08:23 +0300

    added two more tests for the external api

--- test/bound_test.exs
+++ test/bound_test.exs
@@ -16,4 +16,55 @@ defmodule BoundTest do
       assert {:ok, _} = School.Bound.generate_info(46755)
     end
   end
+
+  test "tries to fetch info but returns an error" do
+    expect(GetZipcodeInfoBehaviorMock, :generate_info, fn args ->
+      assert args == nil
+
+      {:ok, %{body: "Not found"}}
+    end)
+
+    assert {:ok, %{body: "Not found"}} = School.Bound.generate_info(nil)
+  end
+
+  test "testing aginst the contents of the body of the response" do
+    expect(GetZipcodeInfoBehaviorMock, :generate_info, fn args ->
+      assert args == 46755
+
+      {:ok,
+       %{
+         "post code": "46755",
+         country: "United States",
+         "country abbreviation": "US",
+         places: [
+           %{
+             "place name": "Kendallville",
+             longitude: "-85.2609",
+             state: "Indiana",
+             "state abbreviation": "IN",
+             latitude: "41.4482"
+           }
+         ]
+       }}
+    end)
+
+    result =
+      {:ok,
+       %{
+         "post code": "46755",
+         country: "United States",
+         "country abbreviation": "US",
+         places: [
+           %{
+             "place name": "Kendallville",
+             longitude: "-85.2609",
+             state: "Indiana",
+             "state abbreviation": "IN",
+             latitude: "41.4482"
+           }
+         ]
+       }}
+
+    assert result = School.Bound.generate_info(46755)
+  end
 end
